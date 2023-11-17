86f4f2824f8183806a4100d7258aa348532c811d
Cunqi Xiao <Cunqi@users.noreply.github.com>
2023-11-16 20:48:46 -0800

    [CXBottomSheet] Add new example target && import SnapKit

---
+++ CXBottomSheet-Example/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CXBottomSheet-Example
+//
+//  Created by Cunqi Xiao on 11/16/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: - Properties
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow()
+        let rootViewController = BottomSheetExampleViewController()
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
---
+++ CXBottomSheet-Example/BottomSheetExampleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BottomSheetExampleViewController.swift
+//  CXBottomSheet-Example
+//
+//  Created by Cunqi Xiao on 11/16/23.
+//
+
+import UIKit
+
+class BottomSheetExampleViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+    }
+    
+}
