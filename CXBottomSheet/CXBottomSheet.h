461e2424273d8239d16cd86e6784e80e6cff07c3
Cunqi Xiao <Cunqi@users.noreply.github.com>
2023-11-16 20:27:00 -0800

    [CXBottomSheet] Initial Commit

---
+++ CXBottomSheet/CXBottomSheet.h
@@ -0,0 +1,18 @@
+//
+//  CXBottomSheet.h
+//  CXBottomSheet
+//
+//  Created by Cunqi Xiao on 11/16/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CXBottomSheet.
+FOUNDATION_EXPORT double CXBottomSheetVersionNumber;
+
+//! Project version string for CXBottomSheet.
+FOUNDATION_EXPORT const unsigned char CXBottomSheetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CXBottomSheet/PublicHeader.h>
+
+
