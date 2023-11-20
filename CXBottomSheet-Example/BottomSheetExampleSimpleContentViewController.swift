c279e7154c29a2e5fbca0132ecc43651e04427b3
Cunqi Xiao <Cunqi@users.noreply.github.com>
2023-11-19 16:22:30 -0800

    [CXBottomSheet] Update simple example

--- CXBottomSheet-Example/BottomSheetExampleSimpleContentViewController.swift
+++ CXBottomSheet-Example/BottomSheetExampleSimpleContentViewController.swift
@@ -7,12 +7,21 @@
 
 import UIKit
 import CXBottomSheet
+import SnapKit
 
 class BottomSheetExampleSimpleContentViewController: UIViewController, CXBottomSheetContentProtocol {
     
+    var bottomSheet: CXBottomSheetProtocol?
+    
     // MARK: - Private properties
     
-    var bottomSheet: CXBottomSheetProtocol?
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.preferredFont(forTextStyle: .headline)
+        label.numberOfLines = 1
+        return label
+    }()
     
     // MARK: - Lifecycles
     
@@ -20,9 +29,18 @@ class BottomSheetExampleSimpleContentViewController: UIViewController, CXBottomS
         super.viewDidLoad()
         
         view.backgroundColor = .systemBlue
+        
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.center.equalTo(view)
+        }
     }
     
     func bottomSheet(didMove bottomSheet: CXBottomSheetProtocol, fromStop: CXBottomSheetStop, toStop: CXBottomSheetStop) {
+        guard let availableHeight = bottomSheet.delegate?.bottomSheet(availableHeightFor: bottomSheet) else {
+            return
+        }
+        label.text = String.init(format: "Current height: %.2f", arguments: [toStop.makeHeight(with: availableHeight)])
     }
     
 }
