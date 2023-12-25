3dee411809d7e9b378cb04858c6960c4b2445e4b
Cunqi Xiao <Cunqi@users.noreply.github.com>
2023-12-24 14:30:06 -1000

    [CXBottomSheet] Integrate with SPM

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CXBottomSheet",
+    platforms: [
+        .iOS(.v15),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "CXBottomSheet",
+            targets: ["CXBottomSheet"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1"))
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "CXBottomSheet",
+            dependencies: ["SnapKit"]),
+        .testTarget(
+            name: "CXBottomSheetTests",
+            dependencies: ["CXBottomSheet"]),
+    ]
+)
